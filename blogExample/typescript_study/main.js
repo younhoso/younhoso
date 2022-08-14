44019ac3f708d2eda38bf1a8e485eabb4e03cdda
younhoso <thdbsgh3443@naver.com>
2022-08-14 14:17:50 +0900

    test: typescript-basic 시작

---
+++ blogExample/typescript_study/main.js
@@ -0,0 +1,7 @@
+console.log('Hello World~');
+var Car = /** @class */ (function () {
+    function Car() {
+        this.engine = 1;
+    }
+    return Car;
+}());
---
+++ blogExample/typescript_study/main.ts
@@ -0,0 +1,7 @@
+console.log('Hello World~')
+class Car {
+	engine: number;
+	constructor() {
+		this.engine = 1;
+	}
+}

99401f82e98c0d0fa418ba477b14e939883bb16f
younhoso <thdbsgh3443@naver.com>
2022-08-19 10:51:48 +0900

    union 정리 및 discriminated 정리

--- blogExample/typescript_study/types_basic/1-4-alias.ts
+++ blogExample/typescript_study/types_basic/1-4-alias.ts
@@ -1,6 +1,8 @@
 {
 	/**
-	 * Type Aliases
+	 * Type Aliases은
+	 * 새로운 타입을 사용자가 정의 할수 있습니다.
+	 * 그리고 원시타입 뿐만 아니라, object 형태도 정의 할수 있습니다. 
 	 */
 	type Text = string;
 	const name: Text = 'Triplex';
@@ -18,6 +20,7 @@
 	/**
 	 * String Literal Types
 	 * 다양한 원시타입 또는 object 또는 실제로 값 자체를 타입으로 결정할수가 있습니다.
+	 * (이것은 union 타입을 사용할때 빛을 바랍니다.) 
 	 */
 	type Name = 'name'; //Name이라는 타입에는 'name'이라는 문자열을 쓴다고 지정 할수 있습니다.
 	let younhoName: Name;

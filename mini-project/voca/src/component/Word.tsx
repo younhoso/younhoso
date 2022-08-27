19858f44bb5d6e940545b40634e6abd7646ec4dc
younhoso <thdbsgh3443@naver.com>
2022-08-28 01:16:03 +0900

    del 삭제 기능 추가

--- mini-project/voca/src/component/Word.tsx
+++ mini-project/voca/src/component/Word.tsx
@@ -12,16 +12,50 @@ export interface IWord {
 	isDone: boolean
 }
 
-function Word({word}: IProps) {
+function Word({word:w}: IProps) {
+	const [word, setWord] = useState(w)
 	const [isShow, setIsShow] = useState(false);
 	const [isDone, setIsDone] = useState(word.isDone);
 
-	function toggleDone() {
-		setIsDone(!isDone)
-	}
-
 	function toggleShow() {
 		setIsShow(!isShow)
+	};
+
+	function toggleDone() {
+		fetch(`http://localhost:3001/words/${word.id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				...word,
+				isDone: !isDone
+			}),
+		})
+		.then(res => {
+			if(res.ok){
+				setIsDone(!isDone);
+			}
+		});
+	};
+
+	function del() {
+		if(window.confirm('삭제 하시겠습니까?')){
+			fetch(`http://localhost:3001/words/${word.id}`, {
+				method: 'DELETE'
+			}).then(res => {
+				if(res.ok){
+					setWord({
+						...word, id: 0
+					});
+				}
+			});
+		}
+	};
+
+	if(word.id === 0){
+		console.log(word)
+		return undefined;
 	}
 
 	return (
@@ -31,7 +65,7 @@ function Word({word}: IProps) {
 			<td>{isShow && word.kor}</td>
 			<td>
 				<button onClick={toggleShow} >뜻 {isShow ? '숨기기' : '보기'}</button>
-				<button className="btn_del">삭제</button>
+				<button onClick={del} className="btn_del">삭제</button>
 			</td>
 		</tr>
 	);

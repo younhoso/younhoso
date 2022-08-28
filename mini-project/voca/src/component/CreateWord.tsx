3f775f374e4a10fa53f9612db9cf54ce0fd35558
younhoso <thdbsgh3443@naver.com>
2022-08-28 12:19:40 +0900

    네트워크 요청 중에는 저장 버튼 중복 못하게 막기

--- mini-project/voca/src/component/CreateWord.tsx
+++ mini-project/voca/src/component/CreateWord.tsx
@@ -1,10 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { useGetFetch } from "../hooks/useFetch";
 
 function CreateWord() {
 	const days = useGetFetch("http://localhost:3001/days");
 	const navigate = useNavigate();
+	const [isLoading, setIsLoading] = useState(false);
 	const engRef = useRef(null);
 	const korRef = useRef(null);
 	const dayRef = useRef(null);
@@ -12,21 +13,25 @@ function CreateWord() {
 	function onSubmit(e: React.FormEvent){
 		e.preventDefault();
 		(async () => {
-			const res = await fetch(`http://localhost:3001/words`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({
-					day: dayRef.current.value,
-					eng: engRef.current.value,
-					kor: korRef.current.value,
-					isDone: false
-				}),
-			})
-			if(res.ok){
-				alert("생성이 완료 되었습니다.");
-				navigate(`/day/${dayRef.current.value}`)
+			if(!isLoading){
+				setIsLoading(true);
+				const res = await fetch(`http://localhost:3001/words`, {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						day: dayRef.current.value,
+						eng: engRef.current.value,
+						kor: korRef.current.value,
+						isDone: false
+					}),
+				})
+				if(res.ok){
+					alert("생성이 완료 되었습니다.");
+					navigate(`/day/${dayRef.current.value}`);
+					setIsLoading(false);
+				}
 			}
 		})()
 	}
@@ -35,11 +40,11 @@ function CreateWord() {
 		<form onSubmit={onSubmit}>
 			<div className="input_area">
 				<label>Eng</label>
-				<input type="text" placeholder='computer' ref={engRef}/>
+				<input type="text" placeholder='computer' ref={engRef} required/>
 			</div>
 			<div className="input_area">
 				<label>Kor</label>
-				<input type="text" placeholder='컴퓨터' ref={korRef}/>
+				<input type="text" placeholder='컴퓨터' ref={korRef} required/>
 			</div>
 			<div className="input_area">
 				<label>Day</label>
@@ -49,7 +54,7 @@ function CreateWord() {
 					))}
 				</select>
 			</div>
-			<button>저장</button>
+			<button style={{opacity: isLoading ? 0.4 : 1}}> {isLoading ? "Saving...": "저장"}</button>
 		</form>
 	);
 }

edf9cb3f6995e9ed851aa50c9f7b916ea565107c
younhoso <thdbsgh3443@naver.com>
2022-09-09 14:28:39 +0900

    인터페이스 변경

--- blogExample/fastcampus_Academy/JavaScript_TypeScript_Essential/Part4 코드/CH04_04_타입과 인터페이스/app.ts
+++ blogExample/fastcampus_Academy/JavaScript_TypeScript_Essential/Part4 코드/CH04_04_타입과 인터페이스/app.ts
@@ -1,31 +1,31 @@
 
-type Store = {
+interface Store {
   currentPage: number;
   feeds: NewsFeed[];
 }
 
-type News = {
-  id: number;
-  time_ago: string;
-  title: string;
-  url: string;
-  user: string;
-  content: string
+interface News {
+  readonly id: number;
+  readonly time_ago: string;
+  readonly title: string;
+  readonly url: string;
+  readonly user: string;
+  readonly content: string
 }
 
-type NewsFeed = News & {
-  comments_count: number;
-  points: number;
+interface NewsFeed extends News {
+  readonly comments_count: number;
+  readonly points: number;
   read?: boolean;
 }
 
-type NewsDetail = News & {
-  comments: NewsComment[];
+interface NewsDetail extends News {
+  readonly comments: NewsComment[];
 }
 
-type NewsComment = News & {
-  comments: NewsComment[];
-  level: number;
+interface NewsComment extends News {
+  readonly comments: NewsComment[];
+  readonly level: number;
 }
 
 const container: HTMLElement | null = document.getElementById('root');
@@ -37,7 +37,7 @@ const store: Store = {
   feeds: [],
 };
 
-function getData<AjaxResponse>(url: string):AjaxResponse {
+function getData<T>(url: string):T {
   ajax.open('GET', url, false);
   ajax.send();
 

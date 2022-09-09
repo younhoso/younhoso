ea9ed78c63f11976af6270df2a09c3bae5f15023
younhoso <thdbsgh3443@naver.com>
2022-09-10 08:35:56 +0900

    element 타입 변경

--- project/component_depots/src/index.tsx
+++ project/component_depots/src/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLAreaElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(<App />);

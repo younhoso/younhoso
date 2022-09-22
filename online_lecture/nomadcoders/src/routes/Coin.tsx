3a9acfbdc6f0bf940b8aa60012b173f67ac3d464
younhoso <thdbsgh3443@naver.com>
2022-09-22 11:47:14 +0900

    data get

--- online_lecture/nomadcoders/src/routes/Coin.tsx
+++ online_lecture/nomadcoders/src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components';
 import { useLocation, useParams } from "react-router";
 
@@ -35,11 +35,27 @@ function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const {state} = useLocation<RouteSate>();
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
+
   return <Container>
           <Header>
             <Title>{state?.name || "Loading..."}</Title>
           </Header>
-          {loading ? <Loader> Loading... </Loader>  : null }
+          {loading ? <Loader> Loading... </Loader> : null }
         </Container>;
 }
 export default Coin;

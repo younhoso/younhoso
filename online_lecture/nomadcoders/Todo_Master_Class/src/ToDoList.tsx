d6bda350f773daa2bc1f5534d6195d1e7ac144fc
younhoso <thdbsgh3443@naver.com>
2022-09-30 22:25:37 +0900

    To Do Setup

--- online_lecture/nomadcoders/Todo_Master_Class/src/ToDoList.tsx
+++ online_lecture/nomadcoders/Todo_Master_Class/src/ToDoList.tsx
@@ -3,9 +3,7 @@ import React, { useState } from "react";
 function ToDoList() {
   const [toDo, setToDo] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
+    const {currentTarget: { value }} = event;
     setToDo(value);
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {

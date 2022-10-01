3439b9f1684e37d84c91ddd7ff4ca56854093db4
younhoso <thdbsgh3443@naver.com>
2022-10-01 19:46:16 +0900

    Form Errors 핸들링

--- online_lecture/nomadcoders/Todo_Master_Class/src/ToDoList.tsx
+++ online_lecture/nomadcoders/Todo_Master_Class/src/ToDoList.tsx
@@ -28,19 +28,34 @@ import { useForm } from "react-hook-form";
 }
 */
 function ToDoList() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState } = useForm();
   const onValid = (data: any) => {
     console.log(data);
   };
+  console.log(formState.errors)
   return (
     <div>
-      <form onSubmit={handleSubmit(onValid)}>
-        <input {...register("Email", { required: true })} placeholder="Email" />
+      <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit(onValid)}>
+        <input {...register("email", { 
+          required: true, 
+          pattern: {
+            value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+            message: "Only naver.com emails allowed"
+          }})} 
+        placeholder="email" 
+        />
         <input {...register("firstName", { required: true })} placeholder="firstName" />
         <input {...register("lastName", { required: true })} placeholder="lastName" />
-        <input {...register("userName", { required: true })} placeholder="userName" />
-        <input {...register("password", { required: true })} placeholder="password" />
-        <input {...register("password1", { required: true })} placeholder="password1" />
+        <input {...register("userName", { required: true, minLength: 10 })} placeholder="userName" />
+        <input {...register("password", { required: true, minLength: 5 })} placeholder="password" />
+        <input {...register("password1",{
+          required: "Password is required",
+          minLength: {
+            value: 5,
+            message: "Your password is too short.",
+          },
+        })} 
+        placeholder="password1" />
         <button>Add</button>
       </form>
     </div>

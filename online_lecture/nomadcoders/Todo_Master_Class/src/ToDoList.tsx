471b66b85ee12f49e997cf3a13ccde2537949644
younhoso <thdbsgh3443@naver.com>
2022-10-01 09:42:02 +0900

    Forms validation 작업

--- online_lecture/nomadcoders/Todo_Master_Class/src/ToDoList.tsx
+++ online_lecture/nomadcoders/Todo_Master_Class/src/ToDoList.tsx
@@ -2,19 +2,25 @@ import React, { useState } from "react";
 
 function ToDoList() {
   const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {currentTarget: { value }} = event;
+    setToDoError("");
     setToDo(value);
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(toDo);
+    if(toDo.length < 10){
+      return setToDoError("To Do should be longer");
+    }
+    console.log("submit");
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
         <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
       </form>
     </div>
   );

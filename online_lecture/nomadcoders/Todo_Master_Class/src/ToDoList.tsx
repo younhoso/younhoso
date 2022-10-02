ab704459d5a1dda3018f3b11ff1ba7f610e37031
younhoso <thdbsgh3443@naver.com>
2022-10-02 09:59:25 +0900

    Form Errors 완료

--- online_lecture/nomadcoders/Todo_Master_Class/src/ToDoList.tsx
+++ online_lecture/nomadcoders/Todo_Master_Class/src/ToDoList.tsx
@@ -27,27 +27,49 @@ import { useForm } from "react-hook-form";
   );
 }
 */
+
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName?: string;
+  userName: string;
+  password: string;
+  password1: string;
+}
+
+const initDefault = {
+  defaultValues: {
+    email: "@naver.com"
+  }
+}
+
 function ToDoList() {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState: {errors} } = useForm<IForm>(initDefault);
   const onValid = (data: any) => {
     console.log(data);
   };
-  console.log(formState.errors)
+  console.log(errors)
+
   return (
     <div>
       <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit(onValid)}>
         <input {...register("email", { 
-          required: true, 
+          required: "Email is required", 
           pattern: {
             value: /^[A-Za-z0-9._%+-]+@naver.com$/,
             message: "Only naver.com emails allowed"
           }})} 
         placeholder="email" 
         />
-        <input {...register("firstName", { required: true })} placeholder="firstName" />
-        <input {...register("lastName", { required: true })} placeholder="lastName" />
-        <input {...register("userName", { required: true, minLength: 10 })} placeholder="userName" />
-        <input {...register("password", { required: true, minLength: 5 })} placeholder="password" />
+        <span>{errors.email?.message}</span>
+        <input {...register("firstName", { required: "write here" })} placeholder="firstName" />
+        <span>{errors.firstName?.message}</span>
+        <input {...register("lastName", { required: "write here" })} placeholder="lastName" />
+        <span>{errors.lastName?.message}</span>
+        <input {...register("userName", { required: "write here", minLength: 10 })} placeholder="userName" />
+        <span>{errors.userName?.message}</span>
+        <input {...register("password", { required: "write here", minLength: 5 })} placeholder="password" />
+        <span>{errors.password?.message}</span>
         <input {...register("password1",{
           required: "Password is required",
           minLength: {
@@ -56,11 +78,11 @@ function ToDoList() {
           },
         })} 
         placeholder="password1" />
+        <span>{errors.password1?.message}</span>
         <button>Add</button>
       </form>
     </div>
   )
 }
-
 
 export default ToDoList; 

687e5b3a0c9ba7195007d86e4a865743b5f30e48
younhoso <thdbsgh3443@naver.com>
2022-10-05 10:29:47 +0900

    Refactoring

---
+++ online_lecture/nomadcoders/Todo_Master_Class/src/components/ToDoList.tsx
@@ -0,0 +1,23 @@
+import { useRecoilValue } from "recoil";
+import { toDoState } from "../atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
+
+
+function ToDoList() {
+  const toDos = useRecoilValue(toDoState);
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <CreateToDo />
+      <ul>
+        {toDos.map((toDo) => {
+          <ToDo key={toDo.id} {...toDo} />
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
--- online_lecture/nomadcoders/Todo_Master_Class/src/main.tsx
+++ online_lecture/nomadcoders/Todo_Master_Class/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { darkTheme } from "./components/theme";
+import { darkTheme } from "./theme";
 
 ReactDOM.render(
   <RecoilRoot>

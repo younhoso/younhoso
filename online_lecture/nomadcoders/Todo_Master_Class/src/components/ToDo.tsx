a7c20777f1e2dcdc27bac5370c41ac62444d71a6
younhoso <thdbsgh3443@naver.com>
2022-10-11 14:42:32 +0900

    Enums

--- online_lecture/nomadcoders/Todo_Master_Class/src/components/ToDo.tsx
+++ online_lecture/nomadcoders/Todo_Master_Class/src/components/ToDo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSetRecoilState } from 'recoil';
-import {IToDo, toDoState} from '../atoms';
+import {Categories, IToDo, toDoState} from '../atoms';
 
 function ToDo({text, category, id}: IToDo) {
 	const setToDos = useSetRecoilState(toDoState);
@@ -23,14 +23,14 @@ function ToDo({text, category, id}: IToDo) {
 	return (
 			<li>
 			 	<span>{text}</span>
-				{ category !== "DOING" && (
-					<button name="DOING" onClick={onClick}>Doing</button> 
+				{ category !== Categories.DOING && (
+					<button name={Categories.DOING} onClick={onClick}>Doing</button> 
 				)}
-				{ category !== "TO_DO" && (
-					<button name="TO_DO" onClick={onClick}>To Do</button>
+				{ category !== Categories.TO_DO && (
+					<button name={Categories.TO_DO} onClick={onClick}>To Do</button>
 				)}
-				{ category !== "DONE" && (
-					<button name="DONE" onClick={onClick}>Done</button> 
+				{ category !== Categories.DONE && (
+					<button name={Categories.DONE} onClick={onClick}>Done</button> 
 				)}
 			</li>
 	)
--- online_lecture/nomadcoders/Todo_Master_Class/src/components/ToDoList.tsx
+++ online_lecture/nomadcoders/Todo_Master_Class/src/components/ToDoList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { toDoSelector, categoryState } from "../atoms";
+import { toDoSelector, categoryState, Categories } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
@@ -9,16 +9,17 @@ function ToDoList() {
   const toDos = useRecoilValue(toDoSelector);
   const onInput = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const {currentTarget: {value}} = event;
-    setCategory(value)
+    setCategory(value as any)
   }
+  console.log(toDos)
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <select value={category} onInput={onInput}>
-        <option value="TO_DO">To Do</option>
-        <option value="DOING">Doing</option>
-        <option value="DONE">Done</option>
+        <option value={Categories.TO_DO}>To Do</option>
+        <option value={Categories.DOING}>Doing</option>
+        <option value={Categories.DONE}>Done</option>
       </select>
       <CreateToDo />
       {toDos?.map(toDo => (

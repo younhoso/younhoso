da2a7471e8e19e65caa665f910c98b633f1d7786
younhoso <thdbsgh3443@naver.com>
2022-10-23 11:01:40 +0900

    Drag and Drop part One

--- online_lecture/nomadcoders/Trello Clone/src/App.tsx
+++ online_lecture/nomadcoders/Trello Clone/src/App.tsx
@@ -1,25 +1,21 @@
-import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { hourSelector, minuteState } from "./atoms";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd"
 
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourSelector);
-  const onMinutesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const {currentTarget: {value}} = event;
-    setMinutes(+value)
-  };
-
-  const onHoursChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const {currentTarget: {value}} = event;
-    setHours(+value)
-  };
-
   return (
-    <div>
-      <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
-      <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours" />
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {() => <ul>
+            <Draggable draggableId="first" index={0}>
+              {() => <li>Hello</li>}
+            </Draggable>
+            <Draggable draggableId="second" index={1}>
+              {() => <li>Two</li>}
+            </Draggable>
+          </ul>}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 }
 

a995fac2507b067df9d1887dfcc610d7b8ac6104
SoYounHo <sojaeheung@sojaeheung-ui-MacBookPro.local>
2022-10-28 15:06:49 +0900

    dec: 주석 수정

--- online_lecture/nomadcoders/Trello Clone/src/atoms.tsx
+++ online_lecture/nomadcoders/Trello Clone/src/atoms.tsx
@@ -1,7 +1,7 @@
 import { atom } from "recoil";
 
 /* default object안에 추가 되거나, 삭제 되거나 동적으로 만들기 위해서
-	이렇게 타입을 지정해줄수 있습니다. */
+	다음과 같이 타입을 지정해줄수 있습니다. */
 interface IToDoState {
 	[key: string]: string[]
 }

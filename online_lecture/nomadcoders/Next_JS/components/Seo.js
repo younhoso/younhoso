0571b7d2d080a7bb6e06dce9e4f16f5f2dd2f11c
younhoso <thdbsgh3443@naver.com>
2022-11-26 20:27:23 +0900

    head title 부분 동적으로 props 받기

--- online_lecture/nomadcoders/Next_JS/components/Seo.js
+++ online_lecture/nomadcoders/Next_JS/components/Seo.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 export default function Seo({ title }) {
   return (
     <Head>
-      <title>{title} | Next Movies</title>
+      <title>{`${title} | Next Movies`}</title>
     </Head>
   );
 }

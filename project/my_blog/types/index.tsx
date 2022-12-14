0745685c4735b0d57d7f6144be53bd23ae54ecf7
younhoso <thdbsgh3443@naver.com>
2022-12-15 00:29:15 +0900

    Add: Notion API get Data

--- project/my_blog/types/index.tsx
+++ project/my_blog/types/index.tsx
@@ -1,3 +1,29 @@
-export interface Props  { 
+export interface Props { 
 	[key: string]: React.ReactNode
+}
+
+export interface PropsReactNodeArr { 
+	[key: string]: React.ReactNode[]
+}
+
+export type DataType = {
+	object:string,
+	id:string,
+	created_time:string,
+	last_edited_time:string,
+	properties: {
+		Date: {
+			id: string,
+			type: string,
+			date: {
+				start: string,
+				end: string,
+				time_zone: null
+			}
+		}
+	}
+}
+
+export type DataTypeResults = {
+	results: DataType[]
 }

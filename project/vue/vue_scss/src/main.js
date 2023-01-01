b3ced82ee56aed4ce52447404ff78824806492fc
unho <thdbsgh3443@kakao.com>
2023-01-01 22:58:18 +0900

    vue 미니 프로젝트

--- project/vue/vue_scss/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import '@/assets/styles/index.scss'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
--- project/vue/vue_scss/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  css: {
-    sourceMap: process.env.NODE_ENV === 'development'
-  }
-});

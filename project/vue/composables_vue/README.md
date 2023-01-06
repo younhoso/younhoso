8885dee08f3e6f1d9c545fa7b142728437a3f928
unho <thdbsgh3443@kakao.com>
2023-01-06 17:01:01 +0900

    hooks -> composables 변경

---
+++ project/vue/composables_vue/README.md
@@ -0,0 +1,7 @@
+# composables_vue
+
+Hook -> composables(컴포저블)
+(vue.js에서는 Hook을 composables라고 합니다.)\
+
+vue의 composables를 동립적으로 제작할 폴더 입니다.\
+vue의 composables들을 FE에서 자주 사용하는 함수들을 시간날때마다 미리 만들어서 재사용을 높이고 작업의 생산성을 높이려는 의도입니다.
---
+++ project/vue/composables_vue/src/components/setup.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    {{stateObject.defaults.default}}
+    <button @click="onClick">이름 변경</button>
+  </div>
+</template>
+
+<script>
+import useObjectState from '@/composables/useObjectState'
+export default {
+  name: 'setup-668643',
+  props: ['useName'],
+  components: {},
+  computed: {},
+  watch: {},
+  data(){
+    return {
+      
+    }
+  },
+  setup({useName}){
+    const datas = {
+      id: 1,
+      defaults: useName
+    }
+    const [stateObject, setStateObject] = useObjectState(datas);
+    return {
+      stateObject,
+      setStateObject
+    }
+  },
+  created(){},
+  mounted(){},
+  unmounted(){},
+  methods: {
+    onClick() {
+      this.setStateObject(this.stateObject.defaults.default = 'test')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

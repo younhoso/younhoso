dfa6535e2f9d6ec9caf66fa27d0d9bad2a1933c5
unho <thdbsgh3443@kakao.com>
2023-01-07 11:43:09 +0900

    Add: notes-study 추가

---
+++ project/vue/Inflearn_Vue/notes-study/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'App-985fe1',
+  components: {},
+  computed: {},
+  watch: {},
+  data(){
+    return {
+      
+    }
+  },
+  setup(){},
+  created(){},
+  mounted(){},
+  unmounted(){},
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
---
+++ project/vue/Inflearn_Vue/notes-study/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>

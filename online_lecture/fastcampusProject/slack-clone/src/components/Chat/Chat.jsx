503420f3259398dedc6859b1b6f86e6ce4584887
younhoso <thdbsgh3443@kakao.com>
2023-09-16 10:11:38 +0900

    채팅 메세지 데이터 보여지기 작업중

--- online_lecture/fastcampusProject/slack-clone/src/components/Chat/Chat.jsx
+++ online_lecture/fastcampusProject/slack-clone/src/components/Chat/Chat.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ChatHeader from './ChatHeader'
 import { Divider, Grid, List, Paper, Toolbar } from '@mui/material'
 import { useSelector } from 'react-redux'
 import ChatInput from './ChatInput';
 import ChatMessage from './ChatMessage';
+import "../../firebase"
+import { child, get, getDatabase, ref } from 'firebase/database';
 
 function Chat() {
-  const { channel } = useSelector((state) => state);
-  console.log(channel)
+  const { channel, user } = useSelector((state) => state);
+  const [messages, setMessages] = useState([]);
 
+  useEffect(() => {
+    if(!channel.currentChannel || !user.currentUser ) return;
+    async function getMessages() {
+      const snapShot = await get(child(ref(getDatabase()), "messages/"+channel.currentChannel.id))
+      setMessages(snapShot.val() ? Object.values(snapShot.val()) : [])
+    }
+    getMessages();
+    return () => {
+      setMessages([])
+    }
+  }, [channel.currentChannel, user.currentUser])
 
   return (
     <>
@@ -16,10 +29,9 @@ function Chat() {
       <ChatHeader channelInfo={channel.currentChannel}/>
       <Grid container component={Paper} variant="outlined" sx={{mt:3, position:'relative'}}>
         <List sx={{height: "calc(100vh - 350px)", overflow:"scroll", width:"100%", position:"relative"}}>
-          {/* 채팅메세지 */}
-          <ChatMessage />
-          <ChatMessage />
-          <ChatMessage />
+          {messages.map(message => (
+            <ChatMessage key={message.timestamp} message={message} user={user}/>
+          ))}
         </List>
         <Divider />
         <ChatInput />

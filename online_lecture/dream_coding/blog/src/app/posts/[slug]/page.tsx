8425fb6df7abebc21f8fdc811f6c7a2d8a95e3fd
younhoso <thdbsgh3443@kakao.com>
2023-09-29 13:50:24 +0900

    update: 리팩토링

--- online_lecture/dream_coding/blog/src/app/posts/[slug]/page.tsx
+++ online_lecture/dream_coding/blog/src/app/posts/[slug]/page.tsx
@@ -1,7 +1,6 @@
-import MarkdownViewer from "@/components/MarkdownViewer";
+import PostContent from "@/components/PostContent";
 import { getPostData } from "@/service/posts";
 import Image from "next/image";
-import { AiTwotoneCalendar } from 'react-icons/ai';
 
 type Props = {
   params: {
@@ -9,8 +8,8 @@ type Props = {
   }
 }
 export default async function PostPage({params: { slug }}: Props) {
-  const {title, description, date, path, content} = await getPostData(slug);
-
+  const post = await getPostData(slug);
+  const {title, path} = post
 
   return (
     <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
@@ -21,16 +20,7 @@ export default async function PostPage({params: { slug }}: Props) {
         width={760}
         height={420}
       />
-      <section className="flex flex-col p-4">
-        <div className="flex items-center self-end text-sky-600">
-          <AiTwotoneCalendar />
-          <p className="font-semibold ml-2">{date.toString()}</p>
-        </div>
-        <h1 className="text-4xl font-bold">{title}</h1>
-        <p className="text-xl font-bold">{description}</p>
-        <div className="w-44 border-2 border-sky-600 mt-4 mb-8" />
-        <MarkdownViewer content={content} />
-      </section>
+      <PostContent post={post}/>
     </article>
   );
 } 

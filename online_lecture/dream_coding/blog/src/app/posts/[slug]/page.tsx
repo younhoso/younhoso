23c3c7da65500d2ca33cc12a3324c4a6d21dea62
younhoso <thdbsgh3443@kakao.com>
2023-09-30 09:59:33 +0900

    update: 비즈니스 로직 수정 작업

--- online_lecture/dream_coding/blog/src/app/posts/[slug]/page.tsx
+++ online_lecture/dream_coding/blog/src/app/posts/[slug]/page.tsx
@@ -9,7 +9,7 @@ type Props = {
 }
 export default async function PostPage({params: { slug }}: Props) {
   const post = await getPostData(slug);
-  const {title, path} = post
+  const {title, path, next, prev} = post
 
   return (
     <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
@@ -21,6 +21,10 @@ export default async function PostPage({params: { slug }}: Props) {
         height={420}
       />
       <PostContent post={post}/>
+      <section>
+        {prev && <p>{prev.title}</p>}
+        {next && <p>{next.title}</p>}
+      </section>
     </article>
   );
 } 
--- online_lecture/dream_coding/blog/src/service/posts.ts
+++ online_lecture/dream_coding/blog/src/service/posts.ts
@@ -10,7 +10,11 @@ export type Post = {
   featured: boolean;
 };
 
-export type PostData = Post & { content: string }
+export type PostData = Post & { 
+  content: string;
+  next: Post | null;
+  prev: Post | null;
+}
 
 export async function getAllPosts(): Promise<Post[]> {
   const filePath = path.join(process.cwd(), 'data', 'posts.json');
@@ -29,12 +33,15 @@ export async function getNonFeaturedPosts(): Promise<Post[]> {
 
 export async function getPostData(fileName: string): Promise<PostData> {
   const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`);
-  const metadata = await getAllPosts()
-  .then((posts) => posts.find((post) => post.path === fileName));
-  if(!metadata){
-    throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
-  }
+  const posts = await getAllPosts();
+  const post = posts.find((post) => post.path === fileName);
 
+  if(!post)throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+
+  const index = posts.indexOf(post);
+  const next = index > 0 ? posts[index - 1] : null;
+  const prev = index < posts.length ? posts[index + 1] : null;
   const content = await readFile(filePath, 'utf-8');
-  return { ...metadata, content };
+  
+  return { ...post, content, next, prev };
 };

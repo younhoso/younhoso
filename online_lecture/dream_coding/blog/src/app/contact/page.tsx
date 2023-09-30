67178584199ac4d5cdc50299e0b104d56a680035
younhoso <thdbsgh3443@kakao.com>
2023-09-30 19:29:34 +0900

    update:어바웃, 연락 페이지 작업

--- online_lecture/dream_coding/blog/src/app/contact/page.tsx
+++ online_lecture/dream_coding/blog/src/app/contact/page.tsx
@@ -1,4 +1,26 @@
+import ContactForm from "@/components/ContactForm";
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+
+const LINKS = [
+  {icon: <AiFillGithub />, url: ''},
+  {icon: <AiFillLinkedin />, url: ''},
+  {icon: <AiFillYoutube />, url: ''},
+];
 export default function ContactPage() {
   
-  return <p>연락하기</p>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+      <p>info@dream-coding.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, idx) => (
+          <a key={idx} href={link.url} target="_blank" rel="noreferrer" className="text-5xl hover:text-yellow-400">
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className="text-3xl font-bold my-8">Or Send me an email</h2>
+      <ContactForm />
+    </section>
+  );
 } 

39d8de16b1b5006ea497fc08b677098586819e92
younhoso <thdbsgh3443@kakao.com>
2023-10-01 10:21:36 +0900

    update: 상태 배너 작업

--- online_lecture/dream_coding/blog/src/components/ContactForm.tsx
+++ online_lecture/dream_coding/blog/src/components/ContactForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { ChangeEvent, FormEvent, useState } from "react";
+import Banner, { BannerData } from "./Banner";
 
 type Form = {
   from: string;
@@ -15,6 +16,8 @@ const baseInfoData = {
 
 export default function ContactForm() {
   const [form, setForm] = useState<Form>(baseInfoData);
+  const [banner, setBanner] = useState<BannerData | null>(null);
+
   const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const {name, value} = e.target;
     setForm(prev => ({...prev, [name]: value}));
@@ -23,22 +26,28 @@ export default function ContactForm() {
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(form)
+    
+    setBanner({message: '성공했어!!', state: 'error'});
+    // setTimeout(() => {
+    //   setBanner(null);
+    // }, 3000);
   };
 
   return (
-    <>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="from">Your Email</label>
-        <input type="email" id="from" name="from" value={form.from} onChange={onChange} required autoFocus />
+    <section className="w-full max-w-md">
+      {banner && <Banner banner={banner} />}
+      <form onSubmit={onSubmit} className="w-full flex flex-col gap-2 my-4 p-4 bg-slate-700 rounded-xl text-white">
+        <label htmlFor="from" className="font-semibold">Your Email</label>
+        <input className="text-black" type="email" id="from" name="from" value={form.from} onChange={onChange} required autoFocus />
 
-        <label htmlFor="subject">Subject</label>
-        <input type="text" id="subject" name="subject" value={form.subject} onChange={onChange} required />
+        <label htmlFor="subject" className="font-semibold" >Subject</label>
+        <input className="text-black" type="text" id="subject" name="subject" value={form.subject} onChange={onChange} required />
 
-        <label htmlFor="message">Message</label>
-        <textarea rows={10} id="message" name="message" value={form.message} onChange={onChange} required />
+        <label htmlFor="message" className="font-semibold">Message</label>
+        <textarea className="text-black" rows={10} id="message" name="message" value={form.message} onChange={onChange} required />
 
-        <button>Submit</button>
+        <button className="bg-yellow-300 text-black font-bold hover:bg-yellow-400">Submit</button>
       </form>
-    </>
+    </section>
   );
 } 

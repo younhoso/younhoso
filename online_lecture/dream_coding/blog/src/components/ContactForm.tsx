fc2422064f776c85c3e51ff97ce5595f49303ede
younhoso <thdbsgh3443@kakao.com>
2023-10-01 09:45:30 +0900

    update: 연락 페이지💡- 입력폼

--- online_lecture/dream_coding/blog/src/components/ContactForm.tsx
+++ online_lecture/dream_coding/blog/src/components/ContactForm.tsx
@@ -1,7 +1,44 @@
+'use client';
+import { ChangeEvent, FormEvent, useState } from "react";
+
+type Form = {
+  from: string;
+  subject: string;
+  message: string;
+};
+
+const baseInfoData = {
+  from: '',
+  subject: '',
+  message: '',
+}
+
 export default function ContactForm() {
-  
+  const [form, setForm] = useState<Form>(baseInfoData);
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const {name, value} = e.target;
+    setForm(prev => ({...prev, [name]: value}));
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form)
+  };
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="from">Your Email</label>
+        <input type="email" id="from" name="from" value={form.from} onChange={onChange} required autoFocus />
+
+        <label htmlFor="subject">Subject</label>
+        <input type="text" id="subject" name="subject" value={form.subject} onChange={onChange} required />
+
+        <label htmlFor="message">Message</label>
+        <textarea rows={10} id="message" name="message" value={form.message} onChange={onChange} required />
+
+        <button>Submit</button>
+      </form>
     </>
   );
 } 

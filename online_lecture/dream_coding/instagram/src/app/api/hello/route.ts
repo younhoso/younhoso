52e8ad6b3d5ac996552a0452f7e0a3f55fe24e19
younhoso <thdbsgh3443@kakao.com>
2023-10-11 09:22:11 +0900

    update:sanity 사용자 정보 스키마 설정

--- online_lecture/dream_coding/instagram/src/app/api/hello/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-  return NextResponse.json('Hello, Next.js!')
-}
-
-// export async function POST() {
-//   return
-// }

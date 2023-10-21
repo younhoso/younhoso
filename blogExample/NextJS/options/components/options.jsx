c1f94795c417fc5c91d2983b733f0bb37465ba85
younhoso <thdbsgh3443@kakao.com>
2023-10-21 16:44:23 +0900

    update: option components 완성 select 이벤트가 발생하면 자식 option에 value값이 전달됨

--- blogExample/NextJS/options/components/options.jsx
+++ blogExample/NextJS/options/components/options.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 export default function Options({combinedObjects}) {
+
+  const [selected, setSelected] = useState({});
+
+  const handleChange = (event, key) => {
+    setSelected({...selected, [key]: event.target.value});
+  };
 
   return (
       <div>
@@ -8,16 +16,17 @@ export default function Options({combinedObjects}) {
                 <strong>{key}</strong>
 
                 <div>
-                  <select defaultValue="default">
-                    <option value="default" disabled>
-                      선택
-                    </option>
-                    {value.map(({value}, idx) => {
-                    return (
-                      <option key={idx}>{value}</option>
+                <select defaultValue="default" onChange={(e) => handleChange(e, key)}>
+                  <option value="default" disabled>
+                    선택
+                  </option>
+                  {(!idx || selected[Object.keys(combinedObjects)[idx-1]]) &&
+                    value.map(({value}, idx) => {
+                      return (
+                        <option key={idx}>{value}</option>
                       )
                     })}
-                  </select>
+                </select>
                 </div>
               </div>
             )

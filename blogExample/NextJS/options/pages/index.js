72d318ba1a6fb5290bd09a450c84e5bb39327b1a
younhoso <thdbsgh3443@kakao.com>
2023-10-23 14:48:26 +0900

    update: 선택한 값의 optionNo를 찾는 재귀 함수 추가

--- blogExample/NextJS/options/pages/index.js
+++ blogExample/NextJS/options/pages/index.js
@@ -1,21 +1,10 @@
-import { useEffect, useState } from 'react';
 import Options from '@/components/options';
 import { useProductOptionStateContext } from '@/context/useProductOptionStateContext';
-import { getCustomFlatOptions, getCustomMultiLevelOptions } from '@/lib/getOptions';
+import { getCustomMultiLevelOptions } from '@/lib/getOptions';
 
 export default function Home() {
-  const [selectOptionNo, setSelectOptionNo] = useState(0);
-  const { originOption, selected, handleChange } = useProductOptionStateContext();
+  const { originOption, selected, selectOptionNo, handleChange } = useProductOptionStateContext();
   const multiLevelOptions = getCustomMultiLevelOptions(originOption.multiLevelOptions);
-
-  useEffect(() => {
-    const combinedString = Object.values(selected).join('|');
-    const optionNo = getCustomFlatOptions(originOption.flatOptions, combinedString);
-    if(optionNo){
-      setSelectOptionNo(optionNo)
-    }
-
-  }, [selected]);
 
   return (
     <>

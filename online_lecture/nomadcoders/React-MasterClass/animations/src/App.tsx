771fc1cdd0b0b9923eb74543b60ac512ad523695
younhoso <thdbsgh3443@kakao.com>
2024-01-06 15:02:34 +0900

    Feat: Variants

--- online_lecture/nomadcoders/React-MasterClass/animations/src/App.tsx
+++ online_lecture/nomadcoders/React-MasterClass/animations/src/App.tsx
@@ -1,21 +1,56 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
+const myVar = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
+
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box
-        initial={{ scale: 0 }}
-        transition={{ type: "spring", delay: 0.5 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-      />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  height: 100vh;
   width: 100vw;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -25,9 +60,20 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
+  width: 70px;
+  height: 70px;
+  place-self: center;
+  border-radius: 35px;
+  background-color: ${(props) => props.theme.colors.white};
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0, 1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 export default App;

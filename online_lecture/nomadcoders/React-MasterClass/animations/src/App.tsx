58cbaf4f7509ff2d1c9c0244d82f41cb69f0048a
younhoso <thdbsgh3443@kakao.com>
2024-01-06 21:32:12 +0900

    Feat: Gestures

--- online_lecture/nomadcoders/React-MasterClass/animations/src/App.tsx
+++ online_lecture/nomadcoders/React-MasterClass/animations/src/App.tsx
@@ -1,49 +1,29 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
-
-const myVar = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
-};
+import { useRef } from "react";
 
 const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
+  hover: { scale: 1.5, rotateZ: 90 },
+  clicl: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 1 } },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileDrag="drag"
+          whileTap="clicl"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
@@ -57,23 +37,22 @@ const Wrapper = styled.div`
   background-color: ${(props) => props.theme.colors.bgColor};
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: ${(props) => props.theme.colors.white};
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Circle = styled(motion.div)`
-  width: 70px;
-  height: 70px;
-  place-self: center;
-  border-radius: 35px;
-  background-color: ${(props) => props.theme.colors.white};
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0, 1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 export default App;

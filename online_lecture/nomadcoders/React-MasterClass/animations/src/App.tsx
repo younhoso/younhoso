cd6b128558eac86cd81a040e9f167204ae609468
younhoso <thdbsgh3443@kakao.com>
2024-01-12 08:45:25 +0900

    Feat: AnimatePresence - Slider part Two

--- online_lecture/nomadcoders/React-MasterClass/animations/src/App.tsx
+++ online_lecture/nomadcoders/React-MasterClass/animations/src/App.tsx
@@ -3,39 +3,47 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 const boxVariants = {
-  initial: {
+  invisible: {
+    x: 500,
     opacity: 0,
     scale: 0,
   },
   visible: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
   },
-  leaving: {
+  exit: {
+    x: -500,
     opacity: 0,
     scale: 0,
-    y: 20,
   },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
 
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>click</button>
       <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box
+              key={i}
+              variants={boxVariants}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
+      <button onClick={prevPlease}>prev</button>
+      <button onClick={nextPlease}>next</button>
     </Wrapper>
   );
 }
@@ -44,6 +52,9 @@ const Wrapper = styled(motion.div)`
   height: 100vw;
   align-items: center;
   background-color: #333;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Box = styled(motion.div)`
@@ -54,6 +65,10 @@ const Box = styled(motion.div)`
   position: absolute;
   top: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
 `;
 
 export default App;

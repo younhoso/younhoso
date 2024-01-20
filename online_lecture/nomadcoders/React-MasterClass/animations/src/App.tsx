6caa33f1acf19a11d65baa3ea4e95af3e4231afe
younhoso <thdbsgh3443@kakao.com>
2024-01-20 16:55:59 +0900

    Fix: motion.div layout

--- online_lecture/nomadcoders/React-MasterClass/animations/src/App.tsx
+++ online_lecture/nomadcoders/React-MasterClass/animations/src/App.tsx
@@ -2,58 +2,22 @@ import styled from "styled-components";
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
-const boxVariants = {
-  entry: (isBack: boolean) => ({
-    x: isBack ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 1,
-    },
-  },
-  exit: (isBack: boolean) => ({
-    x: isBack ? 500 : -500,
-    option: 0,
-    scale: 0,
-    transition: {
-      duration: 1,
-    },
-  }),
-};
-
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [isBack, setIsBack] = useState(false);
-  const prevPlease = () => {
-    setIsBack(true);
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-  };
-  const nextPlease = () => {
-    setIsBack(false);
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  };
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
 
   return (
-    <Wrapper>
-      <AnimatePresence custom={isBack}>
-        <Box
-          custom={isBack}
-          key={visible}
-          variants={boxVariants}
-          initial="entry"
-          animate="center"
-          exit="exit"
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={prevPlease}>prev</button>
-      <button onClick={nextPlease}>next</button>
+    <Wrapper onClick={toggleClicked}>
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 0, scale: 1.5 }} />
+        ) : null}
+      </Box>
     </Wrapper>
   );
 }
@@ -65,6 +29,7 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 20px;
   button {
     background-color: #fff;
     margin-left: 10px;
@@ -76,13 +41,19 @@ const Box = styled(motion.div)`
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 28px;
+`;
+
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 export default App;

12310cb8472a51cb43a207bc74f795901439f274
younhoso <thdbsgh3443@kakao.com>
2024-05-06 19:18:43 +0900

    Feat: tv, search page 생성

---
+++ online_lecture/nomadcoders/Nomflix_Clone/src/app/search/page.tsx
@@ -0,0 +1,7 @@
+import clsx from 'clsx';
+
+import { SearchpageStyled } from '@/styles/pageStyled/SearchpageStyled';
+
+export default function SearchPage() {
+  return <SearchpageStyled className={clsx('page')}></SearchpageStyled>;
+}
---
+++ online_lecture/nomadcoders/Nomflix_Clone/src/app/tv/page.tsx
@@ -0,0 +1,7 @@
+import clsx from 'clsx';
+
+import { TvpageStyled } from '@/styles/pageStyled/TvpageStyled';
+
+export default function TvPage() {
+  return <TvpageStyled className={clsx('page')}></TvpageStyled>;
+}

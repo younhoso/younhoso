3d349be23a0748c4c1f86bf7395acbfbe152eb83
younhoso <thdbsgh3443@kakao.com>
2024-05-31 15:13:32 +0900

    Feat: 첫 커밋

---
+++ project_mini/fastcampus_restaurant/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function HomePage() {
+  return (
+    <h1>Hello World</h1>
+  );
+}

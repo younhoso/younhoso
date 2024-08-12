bfaae376d5aebd78050a9f9e331730fc7576526a
younhoso <thdbsgh3443@kakao.com>
2024-08-12 14:15:53 +0900

    Feat: ReactQueryDevtools 확인

--- online_lecture/fastcampusProject/restaurant-app-project/src/provider/ReactQueryProvider.tsx
+++ online_lecture/fastcampusProject/restaurant-app-project/src/provider/ReactQueryProvider.tsx
@@ -1,5 +1,7 @@
 'use client';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode } from 'react';
 
 export const queryClient = new QueryClient({
@@ -11,6 +13,11 @@ export const queryClient = new QueryClient({
   },
 });
 
-export default function ReactQueryProvider({children}: {children: ReactNode}){
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+export default function ReactQueryProvider({ children }: { children: ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      {children}
+    </QueryClientProvider>
+  );
 }

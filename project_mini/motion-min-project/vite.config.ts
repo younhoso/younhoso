2243b219fc0f21837811cab18fe9394ed622f7ab
younhoso <thdbsgh3443@kakao.com>
2024-08-18 01:29:19 +0900

    Feat: yam 파일 수정

--- project_mini/motion-min-project/vite.config.ts
+++ project_mini/motion-min-project/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/younhoso/project_mini/motion-min-project/dist", // 저장소명을 base로 설정
+  base: "", // 저장소명을 base로 설정
 });

de9af730c525d375594604f7a5261099e35e9644
younhoso <thdbsgh3443@kakao.com>
2024-08-17 16:51:24 +0900

    Docs: motion-min-project 프로젝트 추가

---
+++ project_mini/motion-min-project/README.md
@@ -0,0 +1,2 @@
+# motion-min-project
+TS 기반으만 Single Page Application 만들기
---
+++ project_mini/motion-min-project/src/app.ts
@@ -0,0 +1,18 @@
+import { ImageComponent } from "./components/page/item/image.js";
+import { PageComponent } from "./components/page/page.js";
+
+class App {
+  private readonly page: PageComponent;
+  constructor(appRoot: HTMLElement) {
+    this.page = new PageComponent();
+    this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      "Image Title",
+      "https://picsum.photos/600/300"
+    );
+    image.attachTo(appRoot, "beforeend");
+  }
+}
+
+new App(document.querySelector(".document")! as HTMLElement);

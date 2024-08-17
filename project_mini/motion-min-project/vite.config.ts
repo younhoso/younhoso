e9fbe6449ad1d2a509e9fc25b24707f27826f024
younhoso <thdbsgh3443@kakao.com>
2024-08-17 22:55:59 +0900

    Feat: 배포 test

--- project_mini/motion-min-project/vite.config.ts
+++ project_mini/motion-min-project/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/motion-min-project/", // 저장소명을 base로 설정
-  // 기타 설정...
+  base: "/project_mini/motion-min-project/", // 저장소명을 base로 설정
 });

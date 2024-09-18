c0d69a1e38fa22555128fe4a6e2a63b320857eb5
younhoso <thdbsgh3443@kakao.com>
2024-09-18 15:59:06 +0900

    Feat: Refresh Token 활용하기

--- online_lecture/codeit/project/react-link-service/src/lib/axios.js
+++ online_lecture/codeit/project/react-link-service/src/lib/axios.js
@@ -5,4 +5,14 @@ const instance = axios.create({
   withCredentials: true,
 });
 
+instance.interceptors.response.use(res => res, async (error) => {
+  const originalRequest = error.config;
+  if (error.response?.status === 401 && !originalRequest._retry) {
+    await instance.post('/auth/token/refresh', undefined, { _retry: true });
+    originalRequest._retry = true;
+    return instance(originalRequest);
+  }
+  return Promise.reject(error);
+});
+
 export default instance;

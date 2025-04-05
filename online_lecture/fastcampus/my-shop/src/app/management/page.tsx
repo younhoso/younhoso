cae873b005013f2d06a90361d7992d9502735839
younhoso <thdbsgh3443@kakao.com>
2025-04-05 14:45:48 +0900

    Style:관리자 페이지 스타일 수정

--- online_lecture/fastcampus/my-shop/src/app/management/page.tsx
+++ online_lecture/fastcampus/my-shop/src/app/management/page.tsx
@@ -52,20 +52,34 @@ const ManagementPage = () => {
   return (
     <div className={clsx("flex flex-col items-center p-4")}>
       <div className="w-full max-w-4xl">
-        <p>총 매출액: {stats.totalRevenue}원</p>
-        <p>총 사용자 수: {stats.totalUsers}명</p>
-        <h2 className="text-xl font-bold mt-4">최근 구매 내역</h2>
-        <ul>
-          {stats.recentPurchases.map((purchase, index) => (
-            <li key={index}>
-              <div>
-                <p>{purchase.user_id} </p>
-                <p>{purchase.email} </p>
-              </div>
-              <div>{purchase.amount}원</div>
-            </li>
-          ))}
-        </ul>
+        <div className="bg-white shadow-md rounded-lg p-4 mb-4">
+          <p className="text-lg font-semibold">
+            총 매출액: {stats.totalRevenue}원
+          </p>
+        </div>
+        <div className="bg-white shadow-md rounded-lg p-4 mb-4">
+          <p className="text-lg font-semibold">
+            총 사용자 수: {stats.totalUsers}명
+          </p>
+        </div>
+        <div className="bg-white shadow-md rounded-lg p-4">
+          <h2 className="text-xl font-bold mb-2">최근 구매 내역</h2>
+          <ul>
+            {stats.recentPurchases.map((purchase, index) => (
+              <li key={index} className="border-b last:border-none py-2">
+                <div className="flex justify-between">
+                  <div>
+                    <p className="font-medium">{purchase.user_id}</p>
+                    <p className="text-sm text-gray-600">{purchase.email}</p>
+                  </div>
+                  <div className="text-right">
+                    <p className="text-lg font-semibold">{purchase.amount}원</p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
